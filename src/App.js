351b86c74718c9572ffcdb7556b7dabdf828f041
hyoseon <hyoseon1012@gmail.com>
2019-02-23 17:12:32 +0900

    module 분리

--- src/App.js
+++ src/App.js
@@ -1,69 +1,7 @@
 import React from 'react';
 import './App.css';
-
-const Header = (props) => (
-  <header>
-    <h1>{props.title}</h1>
-    <span className="stats">Players: {props.totalPlayers}</span>
-  </header>
-);
-
-class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      players: 1,
-      score: 0
-    }
-  }
-
-  incrementScore = () => {
-    console.log('ex03. increment', this); // lexical this; 자기 자신을 가르킨다.
-    // this.state.score =1; // 화면 렌더링이 안됨.
-    // this.setState(score:1_
-    /*
-    this.setState(prevState => {
-      return {
-        score: prevState.score + 1
-      }
-    });
-    // 동일구문
-    */
-    this.setState(prevState => ({score: prevState.score + 1}));
-  };
-
-  decrementScore = () => {
-    /*
-    this.setState(prevState => {
-      return {score: prevState.score - 1}
-    });
-    // 동일구문
-    */
-    this.setState(prevState => ({score: prevState.score - 1}));
-  }
-
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> -</button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={this.incrementScore}> +</button>
-      </div>
-    );
-  }
-}
-
-const Player = (props) => (
-  <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>x</button>
-      </span>
-    <span className="player-name">
-        {props.name}
-      </span>
-    <Counter score={props.score}/>
-  </div>
-);
+import {Header} from "./components/Header";
+import {Player} from "./components/Player";
 
 // 1. class 컴포넌트로 변경
 class App extends React.Component {
@@ -78,13 +16,6 @@ class App extends React.Component {
   }
 
   handleRemovePlayer = (id) => {
-    /*
-    this.setState(prevState => {
-      return {
-        players: prevState.players.filter(item => item.id !== id)
-      }
-    })
-    */
     this.setState(prevState => ({players: prevState.players.filter(item => item.id !== id)}))
   }
 
