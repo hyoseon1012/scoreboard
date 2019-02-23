ac965b5f6a987d2e49cfd9bdbc7ffd09d45c7537
hyoseon <hyoseon1012@gmail.com>
2019-02-23 18:18:42 +0900

    count change  lift up

--- src/App.js
+++ src/App.js
@@ -8,15 +8,25 @@ class App extends React.Component {
   //  속성으로 변경
   state = {
     players: [
-      {name: 'LDK', id: 1},
-      {name: 'HONG', id: 2},
-      {name: 'KIM', id: 3},
-      {name: 'PARK', id: 4}
+      {name: 'LDK', id: 1, score: 30},
+      {name: 'HONG', id: 2, score: 40},
+      {name: 'KIM', id: 3, score: 50},
+      {name: 'PARK', id: 4, score: 60}
     ]
   }
 
   handleRemovePlayer = (id) => {
     this.setState(prevState => ({players: prevState.players.filter(item => item.id !== id)}))
+  }
+
+  handleChangeScore = (index, delta) => {
+    const players = this.state.players.map((player, idx) => {
+      if(idx === index) {
+        player.score = player.score + delta;
+      }
+      return player;
+    });
+    this.setState({players});
   }
 
   render() {
@@ -29,8 +39,8 @@ class App extends React.Component {
         {/* JSX를 return 하기때문에 값 세팅은 {} 표현식 사용 */}
         {/* 클래스 컴포넌트는 반드시 this */}
         {/* players값을 자식에게 */}
-        {this.state.players.map(item => <Player id={item.id} name={item.name} score={item.score} key={item.id.toString()}
-                                                removePlayer={this.handleRemovePlayer}/>)}
+        {this.state.players.map((item, index) => <Player id={item.id} name={item.name} score={item.score} key={item.id.toString()} index={index}
+                                                removePlayer={this.handleRemovePlayer} changeScore={this.handleChangeScore}/>)}
       </div>
     );
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,45 +1,16 @@
 import React from 'react';
 
 export class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      players: 1,
-      score: 0
-    }
-  }
-
-  incrementScore = () => {
-    console.log('ex03. increment', this); // lexical this; 자기 자신을 가르킨다.
-    // this.state.score =1; // 화면 렌더링이 안됨.
-    // this.setState(score:1_
-    /*
-    this.setState(prevState => {
-      return {
-        score: prevState.score + 1
-      }
-    });
-    // 동일구문
-    */
-    this.setState(prevState => ({score: prevState.score + 1}));
-  };
-
-  decrementScore = () => {
-    /*
-    this.setState(prevState => {
-      return {score: prevState.score - 1}
-    });
-    // 동일구문
-    */
-    this.setState(prevState => ({score: prevState.score - 1}));
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> -</button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={this.incrementScore}> +</button>
+        <button className="counter-action decrement" onClick={() => this.props.changeScore(this.props.index, -1)}> - </button>
+        <span className="counter-score">{this.props.score}</span>
+        <button className="counter-action increment" onClick={() => this.props.changeScore(this.props.index, +1)}> +</button>
       </div>
     );
   }
